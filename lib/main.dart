7f859455eebf2f53bb801e9269b016179ed47cb8
李禹 <dayu.li@ly.com>
2019-04-02 11:32:23 +0800

    修改视图

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-    const Center(
-      child: const RichText(text: "hello world!"),
-//      child: const RichText("hello world!",textAlign: TextAlign.center,)
-      )
-);
+void main() {
+  //runApp接受的widget将成为widget树的根(ps：相当于顶层容器)
+  runApp(
+    //此处的Center和Text是两个widget,显示效果就是一个Hello, Flutter!在屏幕中央
+    new Center(
+      child: new Text(
+        'Hello, Flutter!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
 
 //void main() => runApp(MyApp());
 
